11c8956d3b547c8888038f032be408da30c92c66
sowaagencia-beep <sowaagencia@gmail.com>
2026-05-06 15:51:01 -0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@
   window.USFORCE_CONFIG = {
     supabase: {
       url: https://lpwdvszmmalvhjeclwtg.supabase.co/rest/v1/
-      anonKey:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxwd2R2c3ptbWFsdmhqZWNsd3RnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwODM5NTUsImV4cCI6MjA5MzY1OTk1NX0.776q23aRQTBO1nGlJxVfVhrjcSF4ak6cbEiNr4-MwdY
+      anonKey: anonKey:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxwd2R2c3ptbWFsdmhqZWNsd3RnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwODM5NTUsImV4cCI6MjA5MzY1OTk1NX0.776q23aRQTBO1nGlJxVfVhrjcSF4ak6cbEiNr4-MwdY
     },
   };
 })();
